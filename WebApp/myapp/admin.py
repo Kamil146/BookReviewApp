d0dc949ed22ebb3fac13fe2eea93e08be23013c5
Kamil146 <kamilgomulka@gmail.com>
2023-06-08 00:27:07 +0200

    Stworzenie modelu danych
    Stworzono model danych składający się z trzech obiektów: Książki, Recenzji i Kategorii.

--- WebApp/myapp/admin.py
+++ WebApp/myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import  Review,Category,Book
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+class ReviewAdmin(admin.ModelAdmin):
+    readonly_fields = ('id','publication_date')
+admin.site.register(Book,BookAdmin)
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Review,ReviewAdmin)
